f399b2e02d507bd926ef9e58edcf589a32f614d7
OddShapedRock <86754685+oddshapedrock@users.noreply.github.com>
2022-08-15 14:47:01 -0500

    update
    update

---
+++ Chapter2/Chapter 2 Programs.py
@@ -0,0 +1,58 @@
+def comment_example():
+    #comment_example recieves no arguments
+    #it explains how to create a function header
+    #and outputs or returns "Hello World!"
+    print("hello world")
+    
+def program2_1():
+    #program 2 1 recieves no arguments
+    #it outputs three messages using apostrophies
+    print ('Kate Austen')
+    print ('123 Full Cirlce Drive')
+    print ('Asheville, NC 2899')
+    
+def program2_2():
+    #program 2 2 recieves no arguments
+    #it outputs three messages using double quotes
+    print ("Kate Austen")
+    print ("123 Full Cirlce Drive")
+    print ("Asheville, NC 2899")
+    
+def program2_3():
+    #program 2 3 recieves no arguments
+    #displays a simple string
+    print ("Don't fear!")
+    print ("I'm Here!")
+    
+def program2_4():
+    #program 2 3 recieves no arguments
+    #displays a simple string
+    print('Your assignment is to read "Hamlet" by tomorrow')
+    
+def program2_5():
+    #program 2 6 recieves no arguments
+    #it outputs three messages using apostrophies
+    #This function displays a person's name and address
+    print ('Kate Austen')
+    print ('123 Full Cirlce Drive')
+    print ('Asheville, NC 2899')
+    
+def program2_6():
+    #program 2 6 recieves no arguments
+    #it outputs three messages using apostrophies
+    #This function displays a person's name and address
+    
+    #Display the Name
+    print ('Kate Austen')
+    #Display the Address
+    print ('123 Full Cirlce Drive')
+    #Display the city, state, and ZIP
+    print ('Asheville, NC 2899')
+    
+def run_all():
+    program2_1()
+    program2_2()
+    program2_3()
+    program2_4()
+    program2_5()
+    program2_6()
--- Chapter2/testProgram.py
+++ Chapter2/testProgram.py
@@ -4,3 +4,13 @@
 def print_something():
     #print a message
     print("This is a message")
+    
+    
+#another function recieves no arguments
+#just outputs two messages seperated by a blank line
+def another_function():
+    #message 1
+    print("This is message 1")
+    print()
+    print("This is message 2")
+    
