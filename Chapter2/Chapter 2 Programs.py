aaebf17ddf3caf2e09f282b640e4efe529805695
Aaron Hamor <2009019@student.goddardusd.com>
2022-08-16 14:46:40 -0500

    update2

--- Chapter2/Chapter 2 Programs.py
+++ Chapter2/Chapter 2 Programs.py
@@ -49,10 +49,76 @@ def program2_6():
     #Display the city, state, and ZIP
     print ('Asheville, NC 2899')
     
-def run_all():
-    program2_1()
-    program2_2()
-    program2_3()
-    program2_4()
-    program2_5()
-    program2_6()
+def program2_7():
+    #program 2 7 recieves no arguments
+    #it outputs a string containing a variable
+    room = "503"
+    print("I am staying in room\n#" + room)
+    
+def program2_8():
+    #program 2 8 recieves no arguments
+    #outputs a string containing two variables
+    top_speed = "160"
+    distance = "300"
+    #Display Message
+    print("The top speed is\n"
+          + top_speed +
+          "\nThe distance traveled is\n"
+          + distance)
+    
+def program2_9():
+    #program 2 9 recieves no arguments
+    #it outputs a string containing a variable
+    room = "503"
+    print("I am staying in room", "#"+room)
+
+def program2_10():
+    #program 2 10 recieves no arguments
+    #it outputs 2 strings with a varibable that changes
+    dollars = "2.70"
+    print("I have", dollars, "in my account.")
+    #reassigning of value
+    dollars = "99.95"
+    print("But now I have", dollars, "in my account!")
+    
+def program2_11():
+    #program 2 11 recieves no arguments
+    #uses variables to output a full name
+    firstName = "Kathryn"
+    lastName = "Marino"
+    print(firstName + " " + lastName)
+
+def program2_12():
+    #program 2 11 recieves no arguments
+    #prompts user for two values and outputs them in string
+    first_name = input("Please enter your first name: ")
+    last_name = input("Please enter your last name: ")
+    print("Hello", first_name, last_name)
+    
+def program2_13():
+    #program 2 13 recieves no arguments
+    #takes user input to display name age and income
+    userName = input("What is your name? : ")
+    userAge = int(input("What is your age? : "))
+    userIncome = float(input("What is your income? : "))
+    print ("Here is what you entered:\n"
+           + "Name:", userName
+           + "\nAge:", userAge,
+            "\nIncome:", userIncome)
+
+def program2_14():
+    #program 2 14 revieves no arguments
+    #adds the values of two variables to create output of 3700
+    salary = 2500
+    bonus = 1200
+    print("Your pay is", pay)
+    
+def program2_15():
+    #program 2 15 revieves no arguments
+    #Gets origional price and take 20% off
+    price = float(input("Origional Price : "))
+    #knocks 20% off
+    price -= (price * .2)
+    print("The sale price is", price)
+    
+    
