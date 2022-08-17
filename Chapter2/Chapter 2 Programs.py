8bb1ccfb0bfd101495aa823ed8a9e1f97df54dc7
OddShapedRock <86754685+oddshapedrock@users.noreply.github.com>
2022-08-17 14:48:03 -0500

    update 3

--- Chapter2/Chapter 2 Programs.py
+++ Chapter2/Chapter 2 Programs.py
@@ -1,3 +1,5 @@
+import turtle
+
 def comment_example():
     #comment_example recieves no arguments
     #it explains how to create a function header
@@ -111,6 +113,7 @@ def program2_14():
     #adds the values of two variables to create output of 3700
     salary = 2500
     bonus = 1200
+    pay = salary + bonus
     print("Your pay is", pay)
     
 def program2_15():
@@ -122,9 +125,70 @@ def program2_15():
     print("The sale price is", price)
     
 def program2_16():
-    #get first test score
-    #get second test score
-    #get thirsd score
-    #calc average
+    #program 2 16 revieves no arguments
+    #gets score 1
+    testScore1 = float(input("Enter test score 1 : "))
+    #adds score 2 to score 1
+    testScore2 = testScore1 + float(input("Enter test score 2 : "))
+    #adds score 3 to the first score then divides by three
+    average = (testScore2 + float(input("Enter test score 3 : "))) / 3
     
+    #output avarage
+    print("Average score is : ", average)
+    
+def program2_17():
+    #program 2 17 recieves no arguments
+    #input seconds
+    inputTime = float(input("Enter the number of seconds: "))
+    #calculations
+    hours = inputTime // 3600
+    minutes = (inputTime // 60) % 60
+    seconds = inputTime % 60
+    #output
+    print("Here is the time in hours, minutes, and seconds:",
+    "\nHours: ", hours,
+    "\nMinutes: ", minutes,
+    "\nSeconds: ", seconds)
+    
+def program2_18():
+    #program 2 18 recieves no arguments
+    #creates variable and prompts for desired amount
+    deposit = (float(input("Enter the desired future value: "))
+    #divides by (1 + input intrest rate) ^ #of years 
+    / (1 + float(input("Enter the annual interest rate: ")))
+    ** float(input("Enter the number of years: ")))
+    #output formated to two decimal places
+    print("You will need to deposit: $" + str(format(deposit, ".2f")))
+    
+def program2_19():
+    #program 2 19 recieves no arguments
+    #math stuff
+    amountDue = 5000
+    monthlyPayment = amountDue / 12
+    #output
+    print("monthly payment is: ", monthlyPayment)
+    
+def program2_20():
+    #program 2 20 recieves no arguments
+    #math stuff
+    amountDue = 5000
+    monthlyPayment = amountDue / 12
+    #output
+    print("monthly payment is: ", end="$")
+    print(format(monthlyPayment, ".2f"))
+    
+def program2_22():
+    #program 2 21 recieves no arguments
+    num1 = 127.90
+    num2 = 3465.15
+    num3 = 3.78
+    num4 = 264.82
+    num5 = 88.08
+    num6 = 800.00
+    print(format(num1, "7.2f"),
+          format(num2, "7.2f"),
+          format(num3, "7.2f"),
+          format(num4, "7.2f"),
+          format(num5, "7.2f"),
+          format(num6, "7.2f"), sep="\n")
     
